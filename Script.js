b59153c66c53e85b6db3298536fb0179aa936644
XiangyuanMao <xiangyuanm1@student.unimelb.edu.au>
2026-06-04 14:42:16 +1000

    create function to add item into the cart

--- Script.js
+++ Script.js
@@ -24,6 +24,8 @@ let prices = [95, 75, 65];
         document.getElementById("quantity" + index).textContent = quantities[index];
 
         updateTotal();
+
+        addCart();
     }
 
     function updateTotal() {
@@ -31,10 +33,78 @@ let prices = [95, 75, 65];
       let total = 0;
 
         for (let i = 0; 
-             i < quantities.length; 
-             i=i+1) {
+             i < quantities.length; i=i+1) {
            total = total + quantities[i] * prices[i];
         }
 
       document.getElementById("total").textContent = "$ " + total;
+      document.getElementById("cart-total").textContent = "$ " + total;
     }
+
+
+function openCart(){
+    document.getElementById("cartOverlay").classList.add("show");
+}
+function closeCart(){
+    document.getElementById("cartOverlay").classList.remove("show");
+}
+
+
+let products = [
+
+    {
+        name:"10 Course Vegan Degustation",
+        price:95,
+        image:"assets/dish1.webp"
+    },
+
+    {
+        name:"Chef's Pick 5 Course",
+        price:75,
+        image:"assets/dish2.webp"
+    },
+
+    {
+        name:"Unlimited Yum Cha",
+        price:65,
+        image:"assets/dish3.webp"
+    }
+
+];
+
+function addCart(){
+
+    let cartItems = document.getElementById("cart-items");
+
+    cartItems.innerHTML = "";
+
+    for(let i=0;i<quantities.length;i=i+1){
+
+        if(quantities[i] > 0){
+
+            cartItems.innerHTML += `
+
+            <div class="cart-item">
+
+                <img src="${products[i].image}"alt="${products[i].name}">
+
+                <div class="cart-info">
+                    <h2>${products[i].name}</h2>
+                    <p>$${products[i].price}</p>
+                </div>
+
+                <div class="cart-quantity">
+
+                    <button onclick= "changeQty(${i},1)"> +</button>
+
+                    <span>${quantities[i]} </span>
+
+                    <button onclick="changeQty(${i},-1)">-</button>
+
+                </div>
+
+            </div>
+            `;
+        }
+    }
+}
